398cae4c464da4d75477b7b69b323bcf0da1d287
Olga Semykina <semikinaolga@mail.ru>
2020-07-13 18:41:02 +0300

    change

--- Mili/src/Mili.java
+++ Mili/src/Mili.java
@@ -1,7 +1,7 @@
 public class Mili {
     public static void main(String[] args) {
-        int price = 7800;
-        int mili = price/20;
+        int price=7800;
+        int mili=price/20;
         System.out.println(mili);
     }
 }
